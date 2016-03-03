f783c83d02cd2f7d8136015c98de337b3532de37
yjgu <364579637@qq.com>
2016-03-03 15:01:24 +0800

    better test srcipt

--- testChangeRequestPeriod.sh
+++ testChangeRequestPeriod.sh
@@ -1,17 +1,34 @@
+echo -e "30ms\t\c"
 ./netLatencyTester $* -rp 30ms
+echo -e "10ms\t\c"
 ./netLatencyTester $* -rp 10ms
+echo -e "3ms\t\c"
 ./netLatencyTester $* -rp 3ms
+echo -e "1ms\t\c"
 ./netLatencyTester $* -rp 1ms
+echo -e "300us\t\c"
 ./netLatencyTester $* -rp 300us
+echo -e "100us\t\c"
 ./netLatencyTester $* -rp 100us
+echo -e "30us\t\c"
 ./netLatencyTester $* -rp 30us
+echo -e "10us\t\c"
 ./netLatencyTester $* -rp 10us
+echo -e "3us\t\c"
 ./netLatencyTester $* -rp 3us
+echo -e "1us\t\c"
 ./netLatencyTester $* -rp 1us
+echo -e "300ns\t\c"
 ./netLatencyTester $* -rp 300ns
+echo -e "100ns\t\c"
 ./netLatencyTester $* -rp 100ns
+echo -e "30ns\t\c"
 ./netLatencyTester $* -rp 30ns
+echo -e "10ns\t\c"
 ./netLatencyTester $* -rp 10ns
+echo -e "3ns\t\c"
 ./netLatencyTester $* -rp 3ns
+echo -e "1ns\t\c"
 ./netLatencyTester $* -rp 1ns
+echo -e "0ns\t\c"
 ./netLatencyTester $* -rp 0ns
--- testChangeRequestSize.sh
+++ testChangeRequestSize.sh
@@ -1,18 +1,35 @@
+echo -e "16\t\c"
 ./netLatencyTester $* -rqs 16
+echo -e "32\t\c"
 ./netLatencyTester $* -rqs 32
-./netLatencyTester $* -rqs 50
-./netLatencyTester $* -rqs 100
-./netLatencyTester $* -rqs 200
-./netLatencyTester $* -rqs 400
-./netLatencyTester $* -rqs 600
-./netLatencyTester $* -rqs 800
+echo -e "64\t\c"
+./netLatencyTester $* -rqs 64
+echo -e "125\t\c"
+./netLatencyTester $* -rqs 125
+echo -e "250\t\c"
+./netLatencyTester $* -rqs 250
+echo -e "500\t\c"
+./netLatencyTester $* -rqs 500
+echo -e "1000\t\c"
 ./netLatencyTester $* -rqs 1000
-./netLatencyTester $* -rqs 1200
-./netLatencyTester $* -rqs 1400
-./netLatencyTester $* -rqs 1600
-./netLatencyTester $* -rqs 1800
+echo -e "2000\t\c"
 ./netLatencyTester $* -rqs 2000
-./netLatencyTester $* -rqs 2200
-./netLatencyTester $* -rqs 2400
-./netLatencyTester $* -rqs 2600
-./netLatencyTester $* -rqs 2800
+echo -e "4000\t\c"
+./netLatencyTester $* -rqs 4000
+echo -e "8000\t\c"
+./netLatencyTester $* -rqs 8000
+echo -e "16000\t\c"
+./netLatencyTester $* -rqs 16000
+echo -e "32000\t\c"
+./netLatencyTester $* -rqs 32000
+echo -e "64000\t\c"
+./netLatencyTester $* -rqs 64000
+echo -e "128000\t\c"
+./netLatencyTester $* -rqs 128000
+echo -e "256000\t\c"
+./netLatencyTester $* -rqs 256000
+echo -e "512000\t\c"
+./netLatencyTester $* -rqs 512000
+echo -e "1024000\t\c"
+./netLatencyTester $* -rqs 1024000
+
