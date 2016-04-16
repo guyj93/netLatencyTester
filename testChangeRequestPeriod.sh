9f4c95c6d928cff85d5e96f2396174f07a55b3c6
eyujugu <yujun.gu@ericsson.com>
2016-04-16 16:47:10 +0800

    test scripts can be executed in other dirs

--- testChangeRequestPeriod.sh
+++ testChangeRequestPeriod.sh
@@ -1,5 +1,6 @@
 rpArray=(10ms 3ms 1ms 300us 100us 30us 10us 3us 1us 300ns 100ns 30ns 10ns 3ns 1ns 0ns)
+scriptPath=$(cd $(dirname "${BASH_SOURCE[0]}");pwd;)
 for rp in ${rpArray[@]}; do
   echo -e "${rp}\t\c"
-  ./netLatencyTester -rp $rp $*
+  $scriptPath/netLatencyTester -rp $rp $*
 done
